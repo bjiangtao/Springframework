1f2119d4d7a7758c75a33ba9b6ae7ab0cbd48508
baijiangtao <775143974@qq.com>
2015-11-20 17:52:36 +0800

    0.1

---
+++ src/main/java/com/pojo/um/User.java
@@ -0,0 +1,18 @@
+package com.pojo.um;
+/**
+ *@author: baijiangtao
+ *@createtime: 2015年11月20日下午3:14:19
+ *@version: V1.0
+ *@Description:
+ */
+public class User {
+	private String id;
+	private String name;
+	private String pwd;
+	private String type;
+	private String locked;
+	private String credit;
+	private String lastVisit;
+	private String lastIp;
+	private String createTime;
+}
